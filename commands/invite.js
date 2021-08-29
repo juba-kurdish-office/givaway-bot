b4173831efdb5d02a3e6797f5c700e28b228e2ea
juba kurdish office <87152369+juba-kurdish-office@users.noreply.github.com>
2021-08-29 14:21:16 +0300

    Create invite.js

---
+++ commands/invite.js
@@ -0,0 +1,19 @@
+const Discord = require('discord.js');
+const config = require('../config.json');
+
+module.exports.run = async (client, message, args) => {
+    let prefix = config.prefix;
+    if (message.author.bot) return;
+    if(!message.content.startsWith(prefix)) return;
+
+    let invite = new Discord.MessageEmbed()
+    .setTitle("Invite & Support Link!")
+    .addField("Support Server", "[Click here to add bot](https://discord.com/api/oauth2/authorize?client_id=881187865470599209&permissions=8&scope=bot)")
+    .setTimestamp()
+    .setFooter(`Requested by ${message.author.tag}`, client.user.displayAvatarURL())
+    message.channel.send(invite);
+}
+
+module.exports.help = {
+    name: "invite"
+}
