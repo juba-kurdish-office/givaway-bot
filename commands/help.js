aef7f8226f82999ee680f07efbd997649cb35125
juba kurdish office <87152369+juba-kurdish-office@users.noreply.github.com>
2021-08-29 15:17:20 +0300

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -11,9 +11,9 @@ module.exports.run = async (client, message, args) => {
       .setTitle("Command List & Guide for the Bot")
       .setDescription("Below are Commands you can do with Bot, Right now there is only 6 commands available, more commands will be added soon.")
       .addField("<a:emoji_84:881198203448475668> Giveaway <a:emoji_84:881198203448475668>","start [channel-name] [Time] [winners] [Prize]\nreroll [prize name]\nend [prize name]")
-      .addField("<a:emoji_2:872570903945441390> Examples", "g!start #giveaway 5m 1 Testing\ng!end Testing\ng!reroll Testing")
-      .addField("<a:emoji_4:872571016612818994> Utility", "ping, invite", true)
-      .addField("<a:emoji_5:872571053086503012> ℹ Information ℹ", "stats", true)
+      .addField("<a:emoji_2:872570903945441390> Examples <a:emoji_2:872570903945441390>", "g!start #giveaway 5m 1 Testing\ng!end Testing\ng!reroll Testing")
+      .addField("<a:emoji_4:872571016612818994> Utility <a:emoji_4:872571016612818994>", "ping, invite", true)
+      .addField("<a:emoji_5:872571053086503012> ℹ Information ℹ <a:emoji_5:872571053086503012>", "stats", true)
       .addField("Check out", "[This Channel](https://www.youtube.com/channel/UCU5OWSH6vuZ864TNuycvMzA) to make your own giveaway bot")
       .setTimestamp()
       .setFooter(`Command Requested By ${message.author.tag}`, client.user.displayAvatarURL());
